240f909d5380a834dce27c1f255e27c64f1885d9
Martín de Amorín <martindeamorin@gmail.com>
2020-11-24 16:16:13 -0300

    create y multer

--- site/controllers/productsController.js
+++ site/controllers/productsController.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 const path = require("path");
+let productsDirname = path.join(__dirname, "/../data/productsData.json");
+let productsData = JSON.parse(fs.readFileSync(productsDirname, "utf-8"));
 
 const productsController = {
     productList : function(req, res, next) {
@@ -14,9 +16,29 @@ const productsController = {
     newProduct : function(req, res, next) {
         res.render('product/productCreateEdit');
     },
-    editProduct : function(req, res, next) {
-        res.render('product/productCreateEdit');
+    createProduct : function(req, res, next){
+        let formData = req.body;
+        formData.imagenProducto = req.files[0].filename;
+        let id = 0;
+        if(productsData[0] == undefined){
+            productsData.push(formData);
+            productsData[0].id = 1;
+        } else{
+        for(let i = 0; i < productsData.length; i++){
+        if(!productsData[i+1]){
+            id = parseInt(productsData[i].id, 10) + 1;
+        }
+        }
+        formData.id = id;
+        productsData.push(formData);
     }
+        let productsDataJSON = JSON.stringify(productsData);
+        fs.writeFileSync(productsDirname, productsDataJSON);
+        res.send("Se ha creado con exito el producto: " + req.body.nombre)
+    },
+    // editProduct : function(req, res, next) {
+    //     res.render('product/productCreateEdit');
+    // }
 };
 
 module.exports = productsController;
--- site/routes/products.js
+++ site/routes/products.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var router = express.Router();
 const productsController = require("../controllers/productsController");
+const path = require("path");
+const multer = require('multer');
+var storage = multer.diskStorage({
+	  destination:(req,file,cb)=>{
+		  cb(null,'public/images/productsImages');
+	  },
+	  filename:(req,file,cb)=>{   
+		  cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+	  }
+    });
+var upload = multer({storage:storage});
 
 /* GET listado productos. */
 router.get('/listadoProductos', productsController.productList);
@@ -11,7 +22,8 @@ router.get('/carrito', productsController.cart);
 router.get('/detalleProducto', productsController.productDetail);
 
 router.get('/productoNuevo', productsController.newProduct);
+router.post('/productoNuevo', upload.any(), productsController.createProduct)
 
-router.get('/productoEditar', productsController.editProduct);
+// router.get('/productoEditar', productsController.editProduct);
 
 module.exports = router;
