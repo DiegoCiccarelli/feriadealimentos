c4b7cf94f73800b26e9d3b1472ee9c25e0b35def
Martín de Amorín <martindeamorin@gmail.com>
2021-03-12 17:04:15 -0300

    seguridad admin

--- site/routes/admin.js
+++ site/routes/admin.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const adminController = require("../controllers/adminController");
+const usersMiddleware = require("../middlewares/usersMiddeware")
 
 /* GET home page. */
-router.get('/menu', adminController.viewAdminMenu);
+router.get('/menu', usersMiddleware.isLogged, usersMiddleware.isAdmin, adminController.viewAdminMenu);
 
 module.exports = router;
